249b2785079e56f0963d469f8804ea9b62512afe
Laptev Roman <romashlapt@mail.ru>
2025-02-23 18:57:51 +0700

    refactor code

--- database_config/config.py
+++ database_config/config.py
@@ -6,11 +6,16 @@ load_dotenv()
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+SESSION_STRING = os.getenv("SESSION_STRING")
 
 DB_CONFIG = {
     "host": os.getenv("DB_HOST"),
     "port": os.getenv("DB_PORT"),
-    "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASSWORD"),
-    "database": os.getenv("DB_NAME"),
+    "user": os.getenv("POSTGRES_USER"),
+    "password": os.getenv("POSTGRES_PASSWORD"),
+    "database": os.getenv("POSTGRES_DB"),
 }
+
+
+
+
--- main.py
+++ main.py
@@ -1,18 +1,12 @@
-import asyncio
 from pyrogram import Client
 from database_config.db import init_db
-from database_config.config import API_ID, API_HASH
+from database_config.config import API_ID, API_HASH, SESSION_STRING
 
-app = Client("my_session", api_id=API_ID, api_hash=API_HASH)
+app = Client("my_session", session_string=SESSION_STRING, api_id=API_ID, api_hash=API_HASH)
 
 async def main():
     await init_db()
-
-    async with app:
-        print("Успешный вход в Telegram!")
-        await app.idle()
-
+    print("Успешный вход в Telegram!")
 
 if __name__ == "__main__":
-    asyncio.run(main())
-
+    app.run(main())
